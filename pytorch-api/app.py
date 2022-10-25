a9734d6f996847ee4416e4158d73a44d78cbfd33
Matías Vergara S <matiasvergara@ug.uchile.cl>
2022-10-25 02:05:07 -0300

    Remove unnecessary imports

--- pytorch-api/app.py
+++ pytorch-api/app.py
@@ -1,6 +1,6 @@
 import json
 from torchvision import models
-from flask import Flask, jsonify, request
+from flask import Flask
 
 app = Flask(__name__)
 imagenet_class_index = json.load(open('imagenet_class_index.json'))
